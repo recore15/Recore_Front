c3472b6975adae5171b55c984a73dafa4fad9fc9
leesol <jbkljo@naver.com>
2022-11-16 19:20:04 +0900

    feat: 검색창 만들기

--- src/page/MainPage.js
+++ src/page/MainPage.js
@@ -10,9 +10,31 @@ const MainPage = () => {
         <img className="logo_main" alt="logo" src="../image/logo.png" />
       </div>
       <div className="btn">
-        <Main_Btn name={"분리수거"} props={"/"} />
-        <Main_Btn name={"음식물쓰레기"} props={"/"} />
-        <Main_Btn name={"에코프렌드"} props={"/"} />
+        <Main_Btn
+          name={"분리수거"}
+          props={"/"}
+          content1={"이름1"}
+          content2={"이름2"}
+          content3={"이름3"}
+        />
+        <Main_Btn
+          name={"음식물쓰레기"}
+          props={"/"}
+          content1={"이름4"}
+          content2={"이름5"}
+          content3={"이름6"}
+        />
+        <Main_Btn
+          name={"에코프렌드"}
+          props={"/"}
+          content1={"이름7"}
+          content2={"이름8"}
+          content3={"이름9"}
+        />
+      </div>
+      <div className="search">
+        <input placeholder="검색어를 입력하시오"></input>
+        <button></button>
       </div>
     </div>
   );
