d7858bd41985f59ef7aed9a82f4972ba1aee20a4
leesol <jbkljo@naver.com>
2022-12-26 15:53:31 +0900

    feat: 까다로운 분리수거 상세페이지1 생성

--- src/page/recycle/Difficult_De.js
+++ src/page/recycle/Difficult_De.js
@@ -1,15 +1,67 @@
 import Menu_Bar from "../../Menu_Bar";
 import Position from "../../Position";
+import { useRef } from "react";
 const Difficult_De = () => {
   return (
     <div className="Difficult_De">
       <Menu_Bar />
       <div className="main">
-        <div className="title">
-          <h1>까다로운 분리수거</h1>
-          <Position title1={"분리수거"} title2={"까다로운 분리수거"} />
+        <div className="content">
+          <div className="title">
+            <div></div>
+            <div>
+              <h1>까다로운 분리수거</h1>
+              <Position title1={"분리수거"} title2={"까다로운 분리수거"} />
+            </div>
+            <div></div>
+          </div>
+          <div className="step">
+            <div className>
+              <h3>배달 음식 먹고 난 후!</h3>
+              <h4>이렇게 분리수거 해주세요</h4>
+            </div>
+            <div className="line"></div>
+            <div>
+              <h4>STEP1.</h4>
+              <h3>비우고</h3>
+            </div>
+            <div className="line"></div>
+            <div>
+              <h4>STEP2.</h4>
+              <h3>씻고</h3>
+            </div>
+            <div className="line"></div>
+            <div>
+              <h4>STEP3.</h4>
+              <h3>분류하고</h3>
+            </div>
+          </div>
+          <div className="de_content">
+            <div className="box">
+              <div className="picture"></div>
+              <div className="writing">
+                <h4>STEP1.</h4>
+                <h3>비우고</h3>
+              </div>
+            </div>
+            <div className="box">
+              <div className="picture"></div>
+              <div className="writing">
+                <h4>STEP2.</h4>
+                <h3>씻고</h3>
+              </div>
+            </div>
+            <div className="box">
+              <div className="picture"></div>
+              <div className="writing">
+                <div>
+                  <h4>STEP3.</h4>
+                </div>
+                <h3>분류하고</h3>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="step"></div>
       </div>
     </div>
   );
