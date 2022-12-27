4aaeefdb9c8a72f7c4dff0fd469fffd51439358e
leesol <jbkljo@naver.com>
2022-12-27 15:50:31 +0900

    feat: detail 페이지 완성 & image_Re 삭제

--- src/page/recycle/Detail.js
+++ src/page/recycle/Detail.js
@@ -1,24 +1,56 @@
+import { useLocation } from "react-router-dom";
 import Menu_Bar from "../../Menu_Bar";
 
 const Detail = () => {
+  const { state } = useLocation();
+  console.log(state);
   return (
     <div className="Detail">
       <Menu_Bar />
       <div className="main">
-        <div className="title">
+        <div>
           <div className="search">
             <input placeholder="검색어를 입력하시오"></input>
             <button></button>
           </div>
-          <div className="title_line">
-            <hr />
+          <div className="line"></div>
+          <div className="title">
+            <h1>재활용 분리 방법 안내</h1>
           </div>
-          <div className="title_menu">
-            <h2>재활용 분리 방법 안내</h2>
+          <div className="content">
+            <img alt={state[0]} src={state[1]} />
+            <div className="name">
+              <h2>{state[0]}</h2>
+            </div>
+          </div>
+          <div className="how">
+            <h2>버리는 방법</h2>
+            <div className="expl">
+              <div className="box">
+                <div className="image">
+                  <img alt={state[2]} src={state[3]} />
+                </div>
+                <h3>step1</h3>
+                <h2>{state[2]}</h2>
+              </div>
+              <div className="box">
+                <div className="image">
+                  <img alt={state[4]} src={state[5]} />
+                </div>
+                <h3>step2</h3>
+                <h2>{state[4]}</h2>
+              </div>
+              <div className="box">
+                <div className="image">
+                  <img alt={state[6]} src={state[7]} />
+                </div>
+                <h3>step3</h3>
+                <h2>{state[6]}</h2>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div className="content"></div>
     </div>
   );
 };
--- src/page/recycle/Guide.js
+++ src/page/recycle/Guide.js
@@ -1,7 +1,8 @@
-import Image_Re from "../../Image/recycle/Image_Re";
+import { useNavigate } from "react-router-dom";
 import Menu_Bar from "../../Menu_Bar";
 
 const Guide = () => {
+  const navigate = useNavigate();
   return (
     <div className="Guide">
       <Menu_Bar />
@@ -19,15 +20,135 @@ const Guide = () => {
           </div>
         </div>
         <div className="content">
-          <dlv className="column1">
-            <Image_Re name={"물품이름"} props={"../Img/temporary.png"} />
-            <Image_Re name={"물품이름"} props={"../Img/temporary.png"} />
-            <Image_Re name={"물품이름"} props={"../Img/temporary.png"} />
-          </dlv>
+          <div className="column1">
+            <div className="box">
+              <button
+                onClick={() => {
+                  navigate("/detail", {
+                    state: [
+                      "물품이름1",
+                      "../Img/temporary.png",
+                      "단계1",
+                      "../Img/temporary.png",
+                      "단계2",
+                      "단계2사진",
+                      "단계3",
+                      "단계3사진",
+                    ],
+                  });
+                }}
+              >
+                <img alt="물품이름1" src="../Img/temporary.png" />
+              </button>
+              <h3>물품이름1</h3>
+            </div>
+            <div className="box">
+              <button
+                onClick={() => {
+                  navigate("/detail", {
+                    state: [
+                      "물품이름2",
+                      "../Img/temporary.png",
+                      "단계1",
+                      "단계1사진",
+                      "단계2",
+                      "단계2사진",
+                      "단계3",
+                      "단계3사진",
+                    ],
+                  });
+                }}
+              >
+                <img alt="물품이름2" src="../Img/temporary.png" />
+              </button>
+              <h3>물품이름2</h3>
+            </div>
+            <div className="box">
+              <button
+                onClick={() => {
+                  navigate("/detail", {
+                    state: [
+                      "물품이름3",
+                      "../Img/temporary.png",
+                      "단계1",
+                      "단계1사진",
+                      "단계2",
+                      "단계2사진",
+                      "단계3",
+                      "단계3사진",
+                    ],
+                  });
+                }}
+              >
+                <img alt="물품이름3" src="../Img/temporary.png" />
+              </button>
+              <h3>물품이름3</h3>
+            </div>
+          </div>
           <div className="column2">
-            <Image_Re name={"물품이름"} props={""} />
-            <Image_Re name={"물품이름"} props={""} />
-            <Image_Re name={"물품이름"} props={""} />
+            <div className="box">
+              <button
+                onClick={() => {
+                  navigate("/detail", {
+                    state: [
+                      "물품이름4",
+                      "물품사진",
+                      "단계1",
+                      "단계1사진",
+                      "단계2",
+                      "단계2사진",
+                      "단계3",
+                      "단계3사진",
+                    ],
+                  });
+                }}
+              >
+                <img alt="물품이름4" src="" />
+              </button>
+              <h3>물품이름4</h3>
+            </div>
+            <div className="box">
+              <button
+                onClick={() => {
+                  navigate("/detail", {
+                    state: [
+                      "물품이름5",
+                      "물품사진",
+                      "단계1",
+                      "단계1사진",
+                      "단계2",
+                      "단계2사진",
+                      "단계3",
+                      "단계3사진",
+                    ],
+                  });
+                }}
+              >
+                <img alt="물품이름5" src="" />
+              </button>
+              <h3>물품이름5</h3>
+            </div>
+            <div className="box">
+              <button
+                onClick={() => {
+                  navigate("/detail", {
+                    state: [
+                      "물품이름6",
+                      "물품사진",
+                      "단계1",
+                      "단계1사진",
+                      "단계2",
+                      "단계2사진",
+                      "단계3",
+                      "단계3사진",
+                    ],
+                  });
+                }}
+              >
+                <img alt="물품이름6" src="" />
+              </button>
+              <h3>물품이름6</h3>
+            </div>
           </div>
         </div>
       </div>
