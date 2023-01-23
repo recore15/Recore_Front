1455fdb531c2e52ed7d95a1caa1181129fafda5a
MinJaeSon <mjsonsj@gmail.com>
2023-01-24 01:23:30 +0900

    feat: 에코플레이스 추가

--- src/page/ecofriend/PlaceMain.jsx
+++ src/page/ecofriend/PlaceMain.jsx
@@ -1,10 +1,59 @@
+import styled from "styled-components";
+import Menu_Bar from "../../Menu_Bar";
+import petImg from "../../Img/ecoplace/pet.png"
+import iceImg from "../../Img/ecoplace/icepack.png"
+
+const PageName = styled.h1`
+    margin-top: 97px;
+    margin-left: 240px;
+`;
+
+const Sub = styled.p`
+    margin-left: 240px;
+    font-size: 16px;
+`;
+
+const Icon = styled.img`
+    width: 35px;
+    height: 35px;
+`;
+
+const Text = styled.p`
+    font-size: 20px;
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-left: 240px;
+    margin-top: 44px;
+`;
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-right: 20px;
+    align-items: center;
+`;
+
 function PlaceMain(props) {
     return (
         <div>
-            
+            <Menu_Bar />
+            <PageName>에코플레이스</PageName>
+            <Sub>아이콘을 클릭하여 수거함의 위치를 확인해보세요! 작은 분리수거 지구 살리기에 큰 효과를 불러옵니다!</Sub>
+            <Wrapper>
+                <Container>
+                    <Icon src={petImg} />
+                    <Text>투명페트병 수거함</Text>
+                </Container>
+                <Container>
+                    <Icon src={iceImg} />
+                    <Text>아이스팩 수거함</Text>
+                </Container>
+            </Wrapper>
         </div>
     )
-
 }
 
 export default PlaceMain;
