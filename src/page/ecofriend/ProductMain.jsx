9c6d91c37c5cee0f14fb78e7a9b9d36bcdecd366
MinJaeSon <mjsonsj@gmail.com>
2023-01-26 16:00:42 +0900

    feat: 친환경샵 css 완성

--- src/page/ecofriend/ProductMain.jsx
+++ src/page/ecofriend/ProductMain.jsx
@@ -1,43 +1,57 @@
 import styled from "styled-components";
 import Menu_Bar from "../../Menu_Bar";
 import rec from "../../Img/Rectangle 64.png";
+import s1 from "../../Img/친환경샵/지구샵로고.png"
+import s1p1 from "../../Img/친환경샵/지구샵_실리콘 비누받침.png"
+import s1p2 from "../../Img/친환경샵/지구샵_유기농그물주머니.png"
+import s1p3 from "../../Img/친환경샵/지구샵_오리진밤(멀티밤).png"
+import s2 from "../../Img/친환경샵/오아시스로고.png"
+import s2p1 from "../../Img/친환경샵/오아시스_방울토마토.png"
+import s2p2 from "../../Img/친환경샵/오아시스_단호박.png"
+import s2p3 from "../../Img/친환경샵/오아시스_시금치.png"
+import s3 from "../../Img/친환경샵/덕분애로고.png"
+import s3p1 from "../../Img/친환경샵/덕분애_스테인리스.png"
+import s3p2 from "../../Img/친환경샵/덕분애_실리콘빨대.png"
+import s3p3 from "../../Img/친환경샵/덕분애_천연대나무밴드.png"
 
 const Wrapper = styled.div`
     margin-bottom: 150px;
 `;
 
 const Container = styled.div`
-    margin-left: 240px;
+    margin-left: 350px;
     margin-top: 113px;
 `;
 
 const Shop = styled.div`
     display: flex;
     flex-direction: row;
-    margin-bottom: 75px;
+    align-items: center;
+    margin-bottom: 44px;
 `;
 
 const ShopImg = styled.img`
     margin-left: 7px;
     margin-right: 10px;
+    height: 52px;
 `;
 
 const ShopText_1 = styled.p`
-    font-size: 16px;
+    font-size: 14px;
     white-space: pre-wrap
     margin: 0px 0px;
-    width: 270px;
+    width: 238px;
 `;
 
 const ShopText_2 = styled.p`
-    font-size: 16px;
+    font-size: 14px;
     white-space: pre-wrap
     margin: 0px 0px;
-    width: 270px;
+    width: 245px;
 `;
 
 const ShopText_3 = styled.p`
-    font-size: 16px;
+    font-size: 14px;
     white-space: pre-wrap
     margin: 0px 0px;
 `;
@@ -45,17 +59,18 @@ const ShopText_3 = styled.p`
 const Product = styled.div`
     display: flex;
     flex-direction: row;
-    margin-left: 272px;
+    margin-left: 40px;
 `;
 
 const Product_D = styled.div`
     display: flex;
     flex-direction: column;
-    margin-right: 240px;
+    margin-right: 40px;
 `;
 
 const PImg = styled.img`
-
+    width: 220px;
+    height: 220px;
 `;
 
 const PName = styled.p`
@@ -78,14 +93,14 @@ function ProductMain(props) {
                 <Container>
                     <Shop>
                         <img src={rec} />
-                        <ShopImg src=""/>
+                        <ShopImg src={s1}/>
                         <ShopText_1>함께 시작하는 제로웨이스트 라이프 하나뿐인 지구를 위한 소비 실천하기</ShopText_1>
                     </Shop>
 
                     <Product>
                         <Product_D>
                             <PImg 
-                                src="" 
+                                src={s1p1}
                                 onClick={() => {
                                     window.open("https://www.yna.co.kr/view/AKR20230112027900003?input=1195m", "_blank");
                                 }}
@@ -95,7 +110,7 @@ function ProductMain(props) {
                         </Product_D>
                         <Product_D>
                             <PImg 
-                                src="" 
+                                src={s1p2}
                                 onClick={() => {
                                     window.open("https://www.yna.co.kr/view/AKR20230112027900003?input=1195m", "_blank");
                                 }}
@@ -105,7 +120,7 @@ function ProductMain(props) {
                         </Product_D>
                         <Product_D>
                             <PImg 
-                                src="" 
+                                src={s1p3}
                                 onClick={() => {
                                     window.open("https://www.yna.co.kr/view/AKR20230112027900003?input=1195m", "_blank");
                                 }}
@@ -119,14 +134,14 @@ function ProductMain(props) {
                 <Container>
                     <Shop>
                         <img src={rec} />
-                        <ShopImg src=""/>
+                        <ShopImg src={s2}/>
                         <ShopText_2>친환경 유기농 신선식품으로 건강하게 오늘밤 주문하면 내일 새벽 도착!</ShopText_2>
                     </Shop>
 
                     <Product>
                         <Product_D>
                             <PImg 
-                                src="" 
+                                src={s2p1}
                                 onClick={() => {
                                     window.open("https://www.yna.co.kr/view/AKR20230112027900003?input=1195m", "_blank");
                                 }}
@@ -136,7 +151,7 @@ function ProductMain(props) {
                         </Product_D>
                         <Product_D>
                             <PImg 
-                                src="" 
+                                src={s2p2}
                                 onClick={() => {
                                     window.open("https://www.yna.co.kr/view/AKR20230112027900003?input=1195m", "_blank");
                                 }}
@@ -146,7 +161,7 @@ function ProductMain(props) {
                         </Product_D>
                         <Product_D>
                             <PImg 
-                                src="" 
+                                src={s2p3}
                                 onClick={() => {
                                     window.open("https://www.yna.co.kr/view/AKR20230112027900003?input=1195m", "_blank");
                                 }}
@@ -160,14 +175,14 @@ function ProductMain(props) {
                 <Container>
                     <Shop>
                         <img src={rec} />
-                        <ShopImg src=""/>
+                        <ShopImg src={s3}/>
                         <ShopText_3>생명과 환경을 회복시키는 제로웨이스트 플랫폼</ShopText_3>
                     </Shop>
 
                     <Product>
                         <Product_D>
                             <PImg 
-                                src="" 
+                                src={s3p1}
                                 onClick={() => {
                                     window.open("https://www.yna.co.kr/view/AKR20230112027900003?input=1195m", "_blank");
                                 }}
@@ -177,7 +192,7 @@ function ProductMain(props) {
                         </Product_D>
                         <Product_D>
                             <PImg 
-                                src="" 
+                                src={s3p2}
                                 onClick={() => {
                                     window.open("https://www.yna.co.kr/view/AKR20230112027900003?input=1195m", "_blank");
                                 }}
@@ -187,7 +202,7 @@ function ProductMain(props) {
                         </Product_D>
                         <Product_D>
                             <PImg 
-                                src="" 
+                                src={s3p3} 
                                 onClick={() => {
                                     window.open("https://www.yna.co.kr/view/AKR20230112027900003?input=1195m", "_blank");
                                 }}
